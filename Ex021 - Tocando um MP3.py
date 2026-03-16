b1a8063308cab2658df1bd94bb9ca33afa8c333c
WesleySantooss <131408363+WesleySantooss@users.noreply.github.com>
2026-03-16 18:35:47 -0300

    Update Ex021 - Tocando um MP3.py

--- Ex021 - Tocando um MP3.py
+++ Ex021 - Tocando um MP3.py
@@ -1,3 +1,9 @@
 import playsound3
 url = 'música.mp3'
 playsound3.playsound(url)
+
+"""Observação:
+
+O arquivo de música não está incluído no repositório por questões de direitos autorais.
+Para executar o exercício, adicione qualquer arquivo `.mp3` na pasta do projeto com o nome `musica.mp3`.
+"""
