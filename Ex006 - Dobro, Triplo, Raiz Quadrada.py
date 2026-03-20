c6d572de0ed942b279b397ce24b3cdbf4e02bf86
WesleySantooss <131408363+WesleySantooss@users.noreply.github.com>
2026-03-20 18:29:48 -0300

    Delete Ex006 - Dobro, Triplo, Raiz Quadrada.py

--- Ex006 - Dobro, Triplo, Raiz Quadrada.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input('Digite um número: '))
-d = n1 * 2
-t = n1 * 3
-r = n1 ** 0.5
-print ('O dobro de {0} vale {1}. \nO triplo de {0} vale {2}. \nA raiz quadrada de {0} é igual a {3:.2f}. '.format(n1, d, t, r))
