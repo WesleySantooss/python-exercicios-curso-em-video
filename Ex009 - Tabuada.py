0ae76c8010d129398bebdf2199196d537bc7aced
WesleySantooss <131408363+WesleySantooss@users.noreply.github.com>
2026-03-20 18:30:12 -0300

    Delete Ex009 - Tabuada.py

--- Ex009 - Tabuada.py
+++
@@ -1,4 +0,0 @@
-n = int(input('Digite um número para ver sua tabuada: '))
-print (f'{'=' * 12} \n{n} x 1 = {(n * 1)}')
-print (f'{n} x 2 = {n * 2} \n{n} x 3 = {n * 3} \n{n} x 4 = {n * 4} \n{n} x 5 = {n * 5}')
-print (f'{n} x 6 = {n * 6} \n{n} x 7 = {n * 7} \n{n} x 8 = {n * 8} \n{n} x 9 = {n * 9} \n{n} x 10 = {n * 10} \n{'_' * 12}')
