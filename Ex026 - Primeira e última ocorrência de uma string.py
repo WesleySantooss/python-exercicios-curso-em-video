11754201ce5db7932c8ef3c2022efcb9a7645dbd
WesleySantooss <131408363+WesleySantooss@users.noreply.github.com>
2026-03-20 18:32:46 -0300

    Delete Ex026 - Primeira e última ocorrência de uma string.py

--- Ex026 - Primeira e última ocorrência de uma string.py
+++
@@ -1,8 +0,0 @@
-frase = str(input('Digite uma frase: ')).upper().strip()
-quantidadea = frase.count('A')
-primeiroa = frase.find('A') + 1
-ultimoa = frase.rfind('A') + 1
-
-print (f'A letra A aparece {quantidadea} vezes na frase.')
-print (f'A primeira letra A apareceu na posição {primeiroa}')
-print (f'A última letra A apareceu na posição {ultimoa}')
